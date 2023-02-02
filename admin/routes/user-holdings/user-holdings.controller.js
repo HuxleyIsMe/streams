4f00609f3ddc3b569ce79069527ba7595bfa97cf
HuxleyIsMe <huxley.millard@gmail.com>
2023-02-02 19:36:19 +0100

    feat: manipulated data into rows

--- admin/routes/user-holdings/user-holdings.controller.js
+++ admin/routes/user-holdings/user-holdings.controller.js
@@ -1,12 +1,41 @@
 const config = require("../../config/default.json");
 const request = require("request");
 
-module.exports.convertInvestmentsToRows = (data) => {};
+// const answer = {
+//     user: '',
+//     fn,
+//     ln,
+//     date,
+//     holding,
+//     value
+// }
 
-module.exports.userHoldingsController = (req, res, next) => {
-  const allInvestments = request.get(
-    `${config.investmentsServiceUrl}/investments`
-  );
+const calculateHoldingValue = (investmentTotal, investmentPercentage) =>
+  investmentTotal * investmentPercentage;
+
+module.exports.convertInvestmentsToRows = (investments) =>
+  investments.reduce((acc, curr) => {
+    curr.holdings.forEach((holding) => {
+      acc.push({
+        userId: curr.userId,
+        firstName: curr.firstName,
+        lastName: curr.lastName,
+        date: curr.date,
+        holding: holding.id,
+        value: calculateHoldingValue(
+          curr.investmentTotal,
+          holding.investmentPercentage
+        ),
+      });
+    });
+    return acc;
+  }, []);
+
+module.exports.userHoldingsController = async (req, res, next) => {
+  const [allInvestments, allHoldings] = await Promise.all([
+    request.get(`${config.investmentsServiceUrl}/investments`),
+    request.get(`${config.financialCompaniesServiceUrl}/companies`),
+  ]);
 
   request.post(`http://localhost:8081/exports`, { holdings: {} });
 };
--- admin/routes/user-holdings/user-holdings.test.js
+++ admin/routes/user-holdings/user-holdings.test.js
@@ -1,14 +1,7 @@
 const controller = require("./user-holdings.controller.js");
 const request = require("request");
 const config = require("../../config/default.json");
-// const isValidJson = (text) => {
-//   try {
-//     JSON.parse(text);
-//     return true;
-//   } catch {
-//     return false;
-//   }
-// };
+
 describe("user-holdings", () => {
   afterEach(() => {
     // restore replaced property
@@ -29,6 +22,76 @@ describe("user-holdings", () => {
   });
 
   describe("user-holdings-helpers", () => {
-    it("converts a list of investments into a row for export", () => {});
+    it("converts a list of investments into a row for export", () => {
+      const dummyData = [
+        {
+          id: "1",
+          userId: "1",
+          firstName: "Billy",
+          lastName: "Bob",
+          investmentTotal: 1400,
+          date: "2020-01-01",
+          holdings: [{ id: "2", investmentPercentage: 1 }],
+        },
+        {
+          id: "2",
+          userId: "2",
+          firstName: "Sheila",
+          lastName: "Aussie",
+          investmentTotal: 20000,
+          date: "2020-01-01",
+          holdings: [
+            { id: "1", investmentPercentage: 0.5 },
+            { id: "2", investmentPercentage: 0.5 },
+          ],
+        },
+        {
+          id: "3",
+          userId: "1",
+          firstName: "Billy",
+          lastName: "Bob",
+          investmentTotal: 1300,
+          date: "2020-02-01",
+          holdings: [{ id: "2", investmentPercentage: 1 }],
+        },
+      ];
+      const result = controller.convertInvestmentsToRows(dummyData);
+      expect(result).toMatchInlineSnapshot(`
+[
+  {
+    "date": "2020-01-01",
+    "firstName": "Billy",
+    "holding": "2",
+    "lastName": "Bob",
+    "userId": "1",
+    "value": 1400,
+  },
+  {
+    "date": "2020-01-01",
+    "firstName": "Sheila",
+    "holding": "1",
+    "lastName": "Aussie",
+    "userId": "2",
+    "value": 10000,
+  },
+  {
+    "date": "2020-01-01",
+    "firstName": "Sheila",
+    "holding": "2",
+    "lastName": "Aussie",
+    "userId": "2",
+    "value": 10000,
+  },
+  {
+    "date": "2020-02-01",
+    "firstName": "Billy",
+    "holding": "2",
+    "lastName": "Bob",
+    "userId": "1",
+    "value": 1300,
+  },
+]
+`);
+    });
   });
 });
