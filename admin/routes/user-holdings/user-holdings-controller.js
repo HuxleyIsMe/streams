8071a6bb28234bc696ba97e71b50d005f80f60dc
HuxleyIsMe <huxley.millard@gmail.com>
2023-02-03 17:45:23 +0100

    docs: update readme 2hrs done now

--- admin/routes/user-holdings/user-holdings-controller.js
+++ admin/routes/user-holdings/user-holdings-controller.js
@@ -5,23 +5,30 @@ import {
   convertInvestmentsToRowsWithHoldings,
 } from "./helpers.js";
 
-export const userHoldingsController = async (req, res, next) => {
-  // change 1.a
-  const [{ data: allInvestments }, { data: allHoldings }] = await Promise.all([
-    axios.get(`${config.investmentsServiceUrl}/investments`),
-    axios.get(`${config.financialCompaniesServiceUrl}/companies`),
-  ]);
+export const userHoldingsController = async (__, res, next) => {
+  try {
+    // change 1.a
+    const [{ data: allInvestments }, { data: allHoldings }] = await Promise.all(
+      [
+        axios.get(`${config.investmentsServiceUrl}/investments`),
+        axios.get(`${config.financialCompaniesServiceUrl}/companies`),
+      ]
+    );
 
-  const holdingsLib = convertHoldersToLib([...allHoldings]);
+    // change 1.b
+    const holdingsLib = convertHoldersToLib([...allHoldings]);
 
-  const investmentsCSV = convertInvestmentsToRowsWithHoldings(
-    allInvestments,
-    holdingsLib
-  );
+    const investmentsCSV = convertInvestmentsToRowsWithHoldings(
+      allInvestments,
+      holdingsLib
+    );
 
-  axios.post(`http://localhost:8081/investments/export`, {
-    holdings: investmentsCSV,
-  });
+    axios.post(`${config.investmentsServiceUrl}/investments/export`, {
+      holdings: investmentsCSV,
+    });
 
-  res.send({ investmentsCSV });
+    res.send({ investmentsCSV });
+  } catch (err) {
+    next(`There was an error! ${err}`);
+  }
 };
