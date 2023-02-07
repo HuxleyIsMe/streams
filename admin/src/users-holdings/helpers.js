9b8d352b1886bd1e261f6a216b800ee278537f66
HuxleyIsMe <huxley.millard@gmail.com>
2023-02-07 14:17:56 +0100

    feat: added helper to convert a chunk of data into a CSV line

--- admin/src/users-holdings/helpers.js
+++ admin/src/users-holdings/helpers.js
@@ -5,6 +5,26 @@ const convertToLookup = ({ arrayOfObjects, key, value }) =>
     return acc;
   }, {});
 
+const calculateHoldingValue = (total, percentage) => total * percentage;
+const transformToCSVWithHolder = ({ investments, holdingLookup }) =>
+  investments.reduce((acc, curr) => {
+    curr.holdings.forEach((holding) => {
+      acc.push([
+        curr.userId,
+        curr.firstName,
+        curr.lastName,
+        curr.date,
+        holdingLookup[holding.id] || holding.id,
+        calculateHoldingValue(
+          curr.investmentTotal,
+          holding.investmentPercentage
+        ),
+      ]);
+    });
+    return acc;
+  }, []);
+
 module.exports = {
   convertToLookup,
+  transformToCSVWithHolder,
 };
--- admin/src/users-holdings/helpers.test.js
+++ admin/src/users-holdings/helpers.test.js
@@ -1,4 +1,4 @@
-const { convertToLookup } = require("./helpers");
+const { convertToLookup, transformToCSVWithHolder } = require("./helpers");
 describe("users holdings helpers", () => {
   describe("convertToLookup", () => {
     it("converts an array of objects to a lookup lib pattern", () => {
@@ -30,7 +30,49 @@ describe("users holdings helpers", () => {
     });
   });
 
-  describe("transformToCsvWithHolder", () => {
-    it("transforms an array of investments to a csv row including the correct holding name", () => {});
+  describe("transformToCSVWithHolder", () => {
+    it.only("transforms an array of investments to a csv row including the correct holding name", () => {
+      const mockedLib = {
+        1: "calvin Harris",
+        2: "pixies",
+      };
+
+      const mockData = [
+        {
+          id: "1",
+          userId: "1",
+          firstName: "Billy",
+          lastName: "Bob",
+          investmentTotal: 1400,
+          date: "2020-01-01",
+          holdings: [{ id: "2", investmentPercentage: 1 }],
+        },
+        {
+          id: "2",
+          userId: "2",
+          firstName: "Sheila",
+          lastName: "Aussie",
+          investmentTotal: 20000,
+          date: "2020-01-01",
+          holdings: [
+            { id: "1", investmentPercentage: 0.5 },
+            { id: "2", investmentPercentage: 0.5 },
+          ],
+        },
+      ];
+
+      const desired = [
+        ["1", "Billy", "Bob", "2020-01-01", "pixies", 1400],
+        ["2", "Sheila", "Aussie", "2020-01-01", "calvin Harris", 10000],
+        ["2", "Sheila", "Aussie", "2020-01-01", "pixies", 10000],
+      ];
+
+      expect(
+        transformToCSVWithHolder({
+          investments: mockData,
+          holdingLookup: mockedLib,
+        })
+      ).toEqual(desired);
+    });
   });
 });
